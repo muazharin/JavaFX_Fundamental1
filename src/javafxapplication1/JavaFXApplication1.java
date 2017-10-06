c627c7eb44fc878632f7fe573d9a86e2dd6c1e4a
muazharin alfan <muazharin alfan>
2017-10-06 16:18:26 +0800

    membuat aplikasi sederhana dengan javafx

--- src/javafxapplication1/JavaFXApplication1.java
+++ src/javafxapplication1/JavaFXApplication1.java
@@ -8,15 +8,13 @@ package javafxapplication1;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 /**
@@ -30,54 +28,62 @@ public class JavaFXApplication1 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Label textJudul = new Label("SKOR");
-        Label textTimA = new Label("Tim A");
-        Label textTimB = new Label("Tim B");
+        //Inisialisasi label dan button
+        Label textJudul = new Label("Court Counter");
+        Label textTimA = new Label("Team A");
+        Label textTimB = new Label("Team B");
         Label vs = new Label("vs");
         Label angkaTimA = new Label("0");
         Label angkaTimB = new Label("0");
-        Button plusTigaA = new Button("+3");
-        Button plusDuaA = new Button("+2");
-        Button plusTigaB = new Button("+3");
-        Button plusDuaB = new Button("+2");
+        Button plusTigaA = new Button("+3 Point");
+        Button plusDuaA = new Button("+2 Point");
+        Button plusTigaB = new Button("+3 Point");
+        Button plusDuaB = new Button("+2 Point");
         Button reset = new Button("RESET");
-        textJudul.setFont(Font.font("Tahoma", FontWeight.BOLD, FontPosture.REGULAR, 20) );
-        vs.setFont(Font.font("Tahoma", FontWeight.BOLD, FontPosture.REGULAR, 20) );
-        textTimA.setFont(Font.font("Tahoma", FontWeight.BOLD, FontPosture.REGULAR, 15) ); 
-        textTimB.setFont(Font.font("Tahoma", FontWeight.BOLD, FontPosture.REGULAR, 15) );
-        angkaTimA.setFont(Font.font("Tahoma", FontWeight.BOLD, FontPosture.REGULAR, 70) );
-        angkaTimB.setFont(Font.font("Tahoma", FontWeight.BOLD, FontPosture.REGULAR, 70) );
-        reset.setFont(Font.font("Tahoma", FontWeight.BOLD, FontPosture.REGULAR, 20) );
+        
+        //membuat id untuk label-label yang ingin diatur khusus
+        textJudul.setId("Judul");
+        angkaTimA.setId("TimA");
+        angkaTimB.setId("TimB");
+        
+        //memaksimalkan lebar value pada label dan button
         textJudul.setMaxWidth(Double.MAX_VALUE);
         plusTigaA.setMaxWidth(Double.MAX_VALUE);
         plusTigaB.setMaxWidth(Double.MAX_VALUE);
         plusDuaA.setMaxWidth(Double.MAX_VALUE);
         plusDuaB.setMaxWidth(Double.MAX_VALUE);
         reset.setMaxWidth(Double.MAX_VALUE);
-        //textJudul.setStyle("-fx-border-color: blue;");
+        //textJudul.setStyle("-fx-color: blue;");
+        
+        //mengatur alignment label judul agar ke tengah
         textJudul.setAlignment(Pos.CENTER);
         reset.setAlignment(Pos.CENTER);
         
+        //proses yg akan terjadi jika tombol +3 untuk tim A ditekan
         plusTigaA.setOnAction((ActionEvent event) -> {
             a=a+3;
             angkaTimA.setText(""+a);
         });
         
+        //proses yg akan terjadi jika tombol +2 untuk tim A ditekan
         plusDuaA.setOnAction((ActionEvent event) -> {
             a=a+2;
             angkaTimA.setText(""+a);
         });
         
+        //proses yg akan terjadi jika tombol +3 untuk tim B ditekan
         plusTigaB.setOnAction((ActionEvent event) -> {
             b=b+3;
             angkaTimB.setText(""+b);
         });
         
+        //proses yg akan terjadi jika tombol +2 untuk tim B ditekan
         plusDuaB.setOnAction((ActionEvent event) -> {
             b=b+2;
             angkaTimB.setText(""+b);
         });
         
+        //proses yg akan terjadi jika tombol reset ditekan
         reset.setOnAction((ActionEvent event) -> {
             a=0;
             b=0;
@@ -86,10 +92,17 @@ public class JavaFXApplication1 extends Application {
         });
         
         GridPane grid = new GridPane();
+        //mengatur agar label menjadi align center pada grid
+        GridPane.setHalignment(textTimA, HPos.CENTER);
+        GridPane.setHalignment(textTimB, HPos.CENTER);
+        GridPane.setHalignment(angkaTimA, HPos.CENTER);
+        GridPane.setHalignment(angkaTimB, HPos.CENTER);
+        
+        grid.setId("pane");
         //grid.setGridLinesVisible(true);
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(10);
-        grid.setHgap(10);
+        grid.setPadding(new Insets(15, 15, 15, 15));
+        grid.setVgap(15);
+        grid.setHgap(15);
         grid.setAlignment(Pos.TOP_CENTER);
         grid.add(textJudul, 1, 0, 9, 1);
         grid.add(textTimA, 0, 3);
@@ -103,7 +116,10 @@ public class JavaFXApplication1 extends Application {
         grid.add(plusDuaB, 10, 9);
         grid.add(reset, 1, 12, 9, 1);
         
-        Scene scene = new Scene(grid, 450, 500) ;
+        
+        Scene scene = new Scene(grid, 500, 645);
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add(JavaFXApplication1.class.getResource("style.css").toExternalForm());
         primaryStage.setTitle("CountCourt");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -113,7 +129,7 @@ public class JavaFXApplication1 extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
     
 }
